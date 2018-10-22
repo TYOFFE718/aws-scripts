3a1df3d484b223e9fdbb3d277c78d2df351d3003
EC2 Default User <ec2-user@ip-192-168-1-41.ec2.internal>
2018-10-22 11:19:17 +0000

    ssh by tag

---
+++ ssh-2-role.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+alias api="ssh -i /home/ec2-user/keys/dev-ec2-instances.pem ec2-user@$(aws ec2 describe-instances --region us-east-1 --filter Name=tag:Name,Values=dev-app-php-api-node --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)"
+alias worker="ssh -i /home/ec2-user/keys/dev-ec2-instances.pem ec2-user@$(aws ec2 describe-instances --region us-east-1 --filter Name=tag:Name,Values=dev-app-php-worker-node --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)"
+alias node="ssh -i /home/ec2-user/keys/dev-ec2-instances.pem ec2-user@$(aws ec2 describe-instances --region us-east-1 --filter Name=tag:Name,Values=dev-audiomack-js-node --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)"
+alias rabbit="ssh -i /home/ec2-user/keys/dev-ec2-instances.pem ec2-user@$(aws ec2 describe-instances --region us-east-1 --filter Name=tag:Name,Values=rabbitmq --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)"
+alias sphinx="ssh -i /home/ec2-user/keys/dev-ec2-instances.pem ec2-user@$(aws ec2 describe-instances --region us-east-1 --filter Name=tag:Name,Values=sphinx --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)"
+alias scylla="ssh -i /home/ec2-user/keys/dev-ec2-instances.pem centos@$(aws ec2 describe-instances --region us-east-1 --filter Name=tag:Name,Values=scylla --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)"
+alias redis="ssh -i /home/ec2-user/keys/dev-ec2-instances.pem ec2-user@$(aws ec2 describe-instances --region us-east-1 --filter Name=tag:Name,Values=redis --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)"
+echo -e "Instance ssh aliases for DEV"
+echo -e "$(alias | grep dev-ec2 | awk -F"=" '{print $1}' | sed 's/alias//g')"
