9b9ceaa562936c1d913e02eae3b52816fc97bed6
Tal Yoffe <talaaronyoffe@gmail.com>
2018-11-20 10:34:49 -0500

    Update ssh-2-role.sh

--- ssh-2-role.sh
+++ ssh-2-role.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#include in user-data
+
 alias api="ssh -i /home/ec2-user/keys/dev-ec2-instances.pem ec2-user@$(aws ec2 describe-instances --region us-east-1 --filter Name=tag:Name,Values=dev-app-php-api-node --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)"
 alias worker="ssh -i /home/ec2-user/keys/dev-ec2-instances.pem ec2-user@$(aws ec2 describe-instances --region us-east-1 --filter Name=tag:Name,Values=dev-app-php-worker-node --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)"
 alias node="ssh -i /home/ec2-user/keys/dev-ec2-instances.pem ec2-user@$(aws ec2 describe-instances --region us-east-1 --filter Name=tag:Name,Values=dev-audiomack-js-node --query "Reservations[*].Instances[*].PrivateIpAddress" --output text)"
